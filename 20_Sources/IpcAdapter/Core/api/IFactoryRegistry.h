36d49e5bd4b3cb381476b91e0530ef8a78cd7b47
Soeren SPROESSIG <ssproessig@gmail.com>
2019-08-31 21:25:15 +0200

    #16    adds doxygen

--- 20_Sources/IpcAdapter/Core/api/IFactoryRegistry.h
+++ 20_Sources/IpcAdapter/Core/api/IFactoryRegistry.h
@@ -14,14 +14,29 @@ namespace IpcAdapter
     {
         FORWARD_DECLARE(IComponent);
 
+        /**
+         * An abstraction of a registry that is used to lookup component factories by component name.
+         *
+         * @author Soeren Sproessig
+         * @version 1
+         */
         class IFactoryRegistry
         {
         public:
             INTERFACE_DESTRUCTOR(IFactoryRegistry)
 
         public:
+            /// a function that creates a shared_ptr of a component
             using FactoryFnc = std::function<std::shared_ptr<IComponent>(void)>;
 
+            /**
+             * Queries the factory for component `aComponentId`.
+             *
+             * @param aComponentId  component id we query a factory for
+             *
+             * @retval  nullptr		if no factory was found
+             * @retval  !=nullptr	if a factory was found for a component identified by `aComponentId`
+             */
             virtual FactoryFnc getFactoryFor(QString const& aComponentId) = 0;
 
         };
--- 20_Sources/IpcAdapter/Core/src/ComponentRegistry.h
+++ 20_Sources/IpcAdapter/Core/src/ComponentRegistry.h
@@ -8,6 +8,12 @@ namespace IpcAdapter
 {
     namespace Core
     {
+        /**
+         * A registry that resolves component factories for the components unique id.
+         *
+         * @author Soeren Sproessig
+         * @version 1
+         */
         class ComponentRegistry:
             IFactoryRegistry
         {
@@ -20,9 +26,27 @@ namespace IpcAdapter
 
             /// @name IFactoryRegistry realization
             /// @{
+            /**
+             * Queries the factory for component `aComponentId`.
+             *
+             * @param aComponentId  component id we query a factory for
+             *
+             * @retval  nullptr		if no factory was found
+             * @retval  !=nullptr	if a factory was found for a component identified by `aComponentId`
+             */
             FactoryFnc getFactoryFor(QString const& aComponentId) override;
             /// @}
 
+            /**
+             * Adds a factory implementation of `aFactoryFunction` to the registry for a component identified by
+             * `aComponentId`.
+             *
+             * @param aComponentId		component id we add the factory for
+             * @param aFactoryFunction	factory function to add
+             *
+             * @retval  true		if the factory was added for `aComponentId`
+             * @retval  false		if inserting the factory failed (e.g. duplicate `aComponentId`)
+             */
             bool addFactoryFor(QString const& aComponentId, FactoryFnc const& aFactoryFunction);
         };
     }
