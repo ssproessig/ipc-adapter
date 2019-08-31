5a2bd1a7b0fb7d5b7fd2d0f875738e7ff635280f
Soeren SPROESSIG <ssproessig@gmail.com>
2019-08-31 18:16:34 +0200

    #7    adding UdpSink doxygen

--- 20_Sources/IpcAdapter/Components/Sinks/src/UdpSink.h
+++ 20_Sources/IpcAdapter/Components/Sinks/src/UdpSink.h
@@ -10,6 +10,12 @@ namespace IpcAdapter
     {
         namespace Sinks
         {
+            /**
+             * An IpcAdapter component that forwards data received from the pipeline to a configured UDP target.
+             *
+             * @author Soeren Sproessig
+             * @version 1
+             */
             class UdpSink
                 : public Core::ISink
             {
@@ -22,11 +28,27 @@ namespace IpcAdapter
 
                 /// @name IComponent realization
                 /// @{
+                /**
+                 * Configures the UdpSink.
+                 *
+                 * Mandatory parameters:
+                 *
+                 * - `host` to send the UDP data to
+                 * - `port` to send the UDP data to
+                 */
                 Core::IConfigurable& getConfigurable() override;
                 /// @}
 
                 /// @name ISink realization
                 /// @{
+                /**
+                 * Sends the data from the `aPipelineFrame` to the configured `host` and `port`.
+                 *
+                 * @param	aPipelineFrame	pipeline frame to extract the data from to send
+                 *
+                 * @retval	true	if data could be sent to `host` and `port` configured
+                 * @retval  false	if data could not be sent successfully
+                 */
                 bool process(Core::IPipelineFrame& aPipelineFrame) override;
                 /// @}
 
