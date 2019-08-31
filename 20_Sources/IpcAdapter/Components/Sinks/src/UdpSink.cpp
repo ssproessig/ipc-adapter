56c6951a638e6819c500c973a92663438d99e6f7
Soeren SPROESSIG <ssproessig@gmail.com>
2019-08-31 18:17:40 +0200

    #7    traces realization and adding scaffolidng for configuring

--- 20_Sources/IpcAdapter/Components/Sinks/src/UdpSink.cpp
+++ 20_Sources/IpcAdapter/Components/Sinks/src/UdpSink.cpp
@@ -1,7 +1,10 @@
 #include "UdpSink.h"
 
 #include "Core/api/IConfigurable.h"
-#include "Core/api/IPipelineStep.h"
+#include "Core/api/IPipelineFrame.h"
+
+#include <QHostAddress>
+#include <QString>
 
 
 
@@ -9,28 +12,57 @@ using IpcAdapter::Components::Sinks::UdpSink;
 
 
 
+namespace
+{
+    namespace Constants
+    {
+        DECLARE_CONST(QString, configHost, QStringLiteral("host"));
+    }
+}
+
+
+
 struct UdpSink::Data
     : IpcAdapter::Core::IConfigurable
 {
-
+    /// @name IConfigurable implementation
+    /// @{
     void onConfigureBegin() override {}
 
     bool doConfigure(QString const& aKey, QString const& aValue) override
     {
-        return false;
+        REALIZE_REQUIREMENT("R-IPCA-SINK-002");
+
+        if(aKey == Constants::configHost())
+        {
+
+        }
+        else
+        {
+            return false;
+        }
+
+        return true;
     }
 
     bool onConfigureEnd() override
     {
         return false;
     }
+    /// @}
+
+
+    QHostAddress targetHost = QHostAddress::LocalHost;
+    quint16 port = 0;
 };
 
 
 
 UdpSink::UdpSink()
     : d(std::make_unique<Data>())
-{}
+{
+    REALIZE_REQUIREMENT("R-IPCA-SINK-001");
+}
 
 UdpSink::~UdpSink() = default;
 
