44941a69c329a4634b2ef9ef4828997ea8ef3248
Soeren SPROESSIG <ssproessig@gmail.com>
2019-08-31 21:16:26 +0200

    #16    adds implementation of ComponentRegistry

--- 20_Sources/IpcAdapter/Core/src/ComponentRegistry.cpp
+++ 20_Sources/IpcAdapter/Core/src/ComponentRegistry.cpp
@@ -1,24 +1,44 @@
 #include "Core/src/ComponentRegistry.h"
 
+#include <QMap>
+
 
 
 using IpcAdapter::Core::ComponentRegistry;
+using FactoryLookupTable = QMap<QString, IpcAdapter::Core::IFactoryRegistry::FactoryFnc>;
 
 
 
-struct ComponentRegistry::Data {};
+struct ComponentRegistry::Data
+{
+    FactoryLookupTable knownFactories;
+};
 
 
 
 IpcAdapter::Core::IFactoryRegistry::FactoryFnc
 ComponentRegistry::getFactoryFor(QString const& aComponentId)
 {
+    auto const& it = d->knownFactories.constFind(aComponentId);
+
+    if(it != d->knownFactories.constEnd())
+    {
+        return *it;
+    }
+
     return nullptr;
 }
 
 
 bool ComponentRegistry::addFactoryFor(QString const& aComponentId, FactoryFnc aFactoryFunction)
 {
+    auto const& it = d->knownFactories.constFind(aComponentId);
+
+    if(it == d->knownFactories.constEnd())
+    {
+        return d->knownFactories.insert(aComponentId, aFactoryFunction) != d->knownFactories.constEnd();
+    }
+
     return false;
 }
 
