2e112c65bbcbf575f8fb01094b8f78e1c5de216e
Soeren SPROESSIG <ssproessig@gmail.com>
2019-09-01 16:24:11 +0200

    #18   clang fix: add missing override

--- 20_Sources/IpcAdapter/Core/src/Runtime.cpp
+++ 20_Sources/IpcAdapter/Core/src/Runtime.cpp
@@ -53,7 +53,7 @@ namespace
             }
         }
 
-        IpcAdapter::Core::IRuntimeConfiguration const& getRuntimeConfiguration() const
+        IpcAdapter::Core::IRuntimeConfiguration const& getRuntimeConfiguration() const override
         {
             return configuration;
         }
