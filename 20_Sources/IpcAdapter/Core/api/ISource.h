13e54e2348be6367f1f6d1e8f95c7664c23c49e0
Soeren SPROESSIG <ssproessig@gmail.com>
2019-09-01 08:05:14 +0200

    #16   introduce yet empty ISource interface

---
+++ 20_Sources/IpcAdapter/Core/api/ISource.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "Shared/api/ConvenienceMacros.h"
+
+#include "Core/api/IComponent.h"
+
+
+
+namespace IpcAdapter
+{
+    namespace Core
+    {
+        class ISource
+            : public IComponent
+        {
+        public:
+            INTERFACE_DESTRUCTOR(ISource)
+        };
+    }
+}
