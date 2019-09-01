db48aa974a58cc3f98a175b7f354cfc7ef5d2dd8
Soeren SPROESSIG <ssproessig@gmail.com>
2019-09-01 14:22:27 +0200

    #18   introducing XML handler stack and the root element handler

--- 20_Sources/IpcAdapter/Core/src/ConfigurationHandler.cpp
+++ 20_Sources/IpcAdapter/Core/src/ConfigurationHandler.cpp
@@ -1,11 +1,13 @@
 #include "ConfigurationHandler.h"
 
+#include <QStack>
 #include <QString>
 #include <QXmlParseException>
 
 
 
 using IpcAdapter::Core::ConfigurationHandler;
+using HandlerStack = QStack<QXmlDefaultHandler*>;
 
 
 
@@ -15,12 +17,43 @@ namespace
     {
         DECLARE_CONST(QString, supportedNamespace, ("urn:ipca-configuration-v1"))
         DECLARE_CONST(QString, errorUnsupportedNamespace, ("configuration has unsupported namespace '%1'!"))
+        DECLARE_CONST(QString, errorUnsupportedRootElement, ("configuration has wrong root element '%1'!"))
     }
+
+
+    struct RootElementHandler: QXmlDefaultHandler
+    {
+        HandlerStack& stack;
+        explicit RootElementHandler(HandlerStack& aStack): stack(aStack) {}
+
+        bool startElement(const QString&, const QString& localName, const QString&, const QXmlAttributes& atts) override
+        {
+            if (localName != "configuration")
+            {
+                throw std::runtime_error(qPrintable(Constants::errorUnsupportedRootElement().arg(localName)));
+            }
+
+            return true;
+        }
+    };
+
+
 }
+
 
 
 struct ConfigurationHandler::Data
 {
+    Data()
+    {
+        handlerStack.append(new RootElementHandler(handlerStack));
+    }
+
+    ~Data()
+    {
+        qDeleteAll(handlerStack);
+    }
+
     bool setErrorStringFrom(QString const& aLevel, const QXmlParseException& anException)
     {
         errorString = QStringLiteral("%1 [%2,%3]: %4")
@@ -29,6 +62,8 @@ struct ConfigurationHandler::Data
                       .arg(anException.message());
         return false;
     }
+
+    HandlerStack handlerStack;
 
     QString errorString;
 };
@@ -55,7 +90,7 @@ bool ConfigurationHandler::startElement
         throw std::runtime_error(qPrintable(Constants::errorUnsupportedNamespace().arg(namespaceURI)));
     }
 
-    return false;
+    return d->handlerStack.top()->startElement(namespaceURI, localName, qName, atts);
 }
 
 
