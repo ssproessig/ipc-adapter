12ee8c19b3c4cd175608f2505bd89ba05f4e0999
Soeren SPROESSIG <ssproessig@gmail.com>
2019-09-01 14:49:32 +0200

    #18   adds all test exceptations for failing component creation

--- 20_Sources/IpcAdapter/Core/tst/RuntimeTest.cpp
+++ 20_Sources/IpcAdapter/Core/tst/RuntimeTest.cpp
@@ -1,5 +1,8 @@
 #include "RuntimeTest.h"
 
+#include "Core/api/IComponent.h"
+#include "Core/api/IConfigurable.h"
+#include "Core/api/GlobalComponentRegistry.h"
 #include "Core/api/Runtime.h"
 #include "Shared/tst/QTestConvenienceMacros.h"
 
@@ -10,10 +13,50 @@ using IpcAdapter::Core::RuntimeTest;
 
 
 
+namespace
+{
+    struct TestComponent
+        : IpcAdapter::Core::IComponent
+        , IpcAdapter::Core::IConfigurable
+    {
+        IConfigurable& getConfigurable() override
+        {
+            return *this;
+        }
+        void onConfigureBegin() override {}
+        bool doConfigure(QString const&, QString const&) override
+        {
+            return acceptParameter;
+        }
+        bool onConfigureEnd() override
+        {
+            return acceptConfiguration;
+        }
+
+        static bool acceptParameter;
+        static bool acceptConfiguration;
+    };
+
+    bool TestComponent::acceptConfiguration = true;
+    bool TestComponent::acceptParameter = true;
+
+    REGISTER_COMPONENT(TestComponent);
+}
+
+
+
 RuntimeTest::RuntimeTest()
 {
     TEST_SPEC_META_INFORMATION("Soeren Sproessig");
     TEST_REQUIREMENT("R-IPCA-SINK-001");
+}
+
+
+
+void RuntimeTest::init()
+{
+    TestComponent::acceptConfiguration = true;
+    TestComponent::acceptParameter = true;
 }
 
 
@@ -68,5 +111,53 @@ void RuntimeTest::test_05_Runtime_initialization_fails_for_root_element_with_uns
         Runtime::createFrom(":/RuntimeTest_05_root_element_with_wrong_version.xml");,
         "creating Runtime from XML with correct schema and root element, but unsupported version, must fail",
         "configuration has unsupported version '2'!"
+    );
+}
+
+
+
+void RuntimeTest::test_06_Runtime_initialization_fails_if_unknown_component_shall_be_created()
+{
+    EXPECT_EXCEPTION(
+        Runtime::createFrom(":/RuntimeTest_06_unknown_component.xml");,
+        "creating Runtime from configuration that references unknown component must fail",
+        "unknown component 'UnknownComponent' requested!"
+    );
+}
+
+
+
+void RuntimeTest::test_07_Runtime_initialization_fails_if_duplicate_component_shall_be_created()
+{
+    EXPECT_EXCEPTION(
+        Runtime::createFrom(":/RuntimeTest_07_duplicate_component_id.xml");,
+        "creating Runtime from configuration that configures the same id twice MUST fail",
+        "unable to create 'TestComponent' again for id 'id1'!"
+    );
+}
+
+
+
+void RuntimeTest::test_08_Runtime_initialization_fails_if_component_fails_to_configure()
+{
+    TestComponent::acceptConfiguration = false;
+
+    EXPECT_EXCEPTION(
+        Runtime::createFrom(":/RuntimeTest_08_two_components.xml");,
+        "creating Runtime from configuration must fail if component created fails to configure",
+        "unable to finish configuring 'id1'!"
+    );
+}
+
+
+
+void RuntimeTest::test_09_Runtime_initialization_fails_if_component_rejects_parameter()
+{
+    TestComponent::acceptParameter = false;
+
+    EXPECT_EXCEPTION(
+        Runtime::createFrom(":/RuntimeTest_08_two_components.xml");,
+        "creating Runtime from configuration must fail if component rejects a parameter",
+        "component 'id1' rejects parameter 'aKey' with value 'aValue'!"
     );
 }
--- 20_Sources/IpcAdapter/Core/tst/RuntimeTest.h
+++ 20_Sources/IpcAdapter/Core/tst/RuntimeTest.h
@@ -16,11 +16,17 @@ namespace IpcAdapter
             RuntimeTest();
 
         private slots:
+            void init();
             void test_01_Runtime_initialization_fails_if_not_existing_file_is_passed();
             void test_02_Runtime_initialization_fails_for_broken_file();
             void test_03_Runtime_initialization_fails_for_wrong_xml_used();
             void test_04_Runtime_initialization_fails_for_wrong_root_element();
             void test_05_Runtime_initialization_fails_for_root_element_with_unsupported_version();
+
+            void test_06_Runtime_initialization_fails_if_unknown_component_shall_be_created();
+            void test_07_Runtime_initialization_fails_if_duplicate_component_shall_be_created();
+            void test_08_Runtime_initialization_fails_if_component_fails_to_configure();
+            void test_09_Runtime_initialization_fails_if_component_rejects_parameter();
 
         };
     }
