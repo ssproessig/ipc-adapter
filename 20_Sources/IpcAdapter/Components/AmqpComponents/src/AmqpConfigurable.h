d2a89c5d68d3813db72f0c4bff28fa54e85b2c79
Soeren SPROESSIG <ssproessig@gmail.com>
2019-10-06 18:16:15 +0200

    #20   adds doxygen to public DTO and its configurable

--- 20_Sources/IpcAdapter/Components/AmqpComponents/src/AmqpConfigurable.h
+++ 20_Sources/IpcAdapter/Components/AmqpComponents/src/AmqpConfigurable.h
@@ -14,6 +14,12 @@ namespace IpcAdapter
         {
             FORWARD_DECLARE_STRUCT(AmqpConfiguration);
 
+            /**
+             * An AMQP configuration handler.
+             *
+             * @author Soeren Sproessig
+             * @version 1
+             */
             class AmqpConfigurable
                 : public Core::IConfigurable
             {
@@ -22,17 +28,59 @@ namespace IpcAdapter
                 PIMPLED_DATA(d);
 
             public:
+                /// a callback to invoke once configuration finished
                 using ConfigurationFinishedCallback = std::function<void()>;
 
+                /**
+                 * Initializes the `AmqpConfigurable`.
+                 *
+                 * @param aConfiguration    configuration to store configuration
+                 * @param aCallback         callback to invoke after configuration finished successfully
+                 */
                 AmqpConfigurable(AmqpConfiguration& aConfiguration, ConfigurationFinishedCallback const& aCallback);
                 ~AmqpConfigurable() override;
 
                 /// @name IConfigurable implementation
                 /// @{
+                /**
+                 * Begins configuring and initializes the referenced `AmqpConfigurable` to its default values.
+                 */
                 void onConfigureBegin() override;
 
+                /**
+                 * Configures the given parameter `aKey` to `aValue`.
+                 *
+                 * Connection parameters
+                 * - `amqp.host`            AMQP host to connect to
+                 * - `amqp.port`            AMQP port to connect to
+                 *
+                 * Authentication parameters
+                 * - `amqp.user`            AMQP user
+                 * - `amqp.pwd`             AMQP password
+                 * - `amqp.vhost`           AMQP virtual host
+                 *
+                 * Exchange parameters
+                 * - `exchange.name`        AMQP exchange name
+                 * - `exchange.type`        AMQP exchange type (supported `direct`, `fanout`, `topic`)
+                 *
+                 * Publication parameters
+                 * - `exchange.routing-key` routing-key to publish messages under
+                 *                          use meta-data placeholders `${key}` to replace them with their value
+                 *
+                 * @param   aKey    parameter to configure
+                 * @param   aValue  value to configure the parameter to
+                 *
+                 * @retval  true    parameter `aKey` was successfully configured to `aValue`
+                 * @retval  false   parameter `aKey` failed to configure
+                 */
                 bool doConfigure(QString const& aKey, QString const& aValue) override;
 
+                /**
+                 * Finishes configuring the `AmqpConfigurable`.
+                 *
+                 * @retval  true    configuration finished successfully
+                 * @retval  false   configuration failed
+                 */
                 bool onConfigureEnd() override;
                 /// @}
             };
--- 20_Sources/IpcAdapter/Components/AmqpComponents/src/AmqpConfiguration.h
+++ 20_Sources/IpcAdapter/Components/AmqpComponents/src/AmqpConfiguration.h
@@ -11,33 +11,77 @@ namespace IpcAdapter
     {
         namespace AmqpComponents
         {
+            /**
+             * AMQP configuration "data transfer object"
+             *
+             * @author Soeren Sproessig
+             * @version 1
+             */
             struct AmqpConfiguration
             {
+                /// @name AMQP connection parameters
+                /// @{
+                /// AMQP host to connect to
                 QHostAddress host;
+                /// AMQP port to connect to
                 quint16 port = 0;
+                /// @}
 
+                /// @name AMQP authentication parameters
+                /// @{
+                /// AMQP user to authenticate with
                 QString user;
+                /// AMQP password to authenticate with
                 QString pwd;
+                /// AMQP virtual host to authenticate at
                 QString vhost;
+                /// @}
 
+                /// @name AMQP exchange parameters
+                /// @{
+                /// AMQP exchange to use
                 QString exchangeName;
+                /// AMQP exchange type to use
                 QString exchangeType;
+                /// @}
 
+                /// @name AMQP publication parameters
+                /// @{
+                /// AMQP routing-key to publish messages under
                 QString routingKey;
+                /// @}
 
+                /// flag that carries whether the `AmqpConfigurable` configured the `AmqpConfiguration` successfully
                 bool isOk = false;
 
 
+                /**
+                 * Creates the `AmqpConfiguration` and initializes it to the required default values.
+                 */
                 AmqpConfiguration();
 
+                /**
+                 * Initializes the `AmqpConfiguration` to its defined default values.
+                 */
                 void initDefaults();
 
+
+                /// Uri style to return AMQP URI in
                 enum class UriStyle : bool
                 {
+                    /// returns the URI ready for usage
                     ForUsage,
+                    /// returns the URI with password replaced with `***`
                     LogSafe
                 };
 
+                /**
+                 * Returns the AMQP URI from the `AmqpConfiguration`.
+                 *
+                 * @param anUriStyle    style to return the AMQP URI in
+                 *
+                 * @return AMQP URI from the configuration
+                 */
                 QString getAmqpUri(UriStyle const anUriStyle) const;
             };
         }
